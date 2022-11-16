628867d5bff2162fb11e9ec4d758509c205fd2ef
desktop <jallen108@gsu.edu>
2022-11-16 10:02:59 -0500

    initial commit

---
+++ getting-staRted/practice-script.R
@@ -0,0 +1,14 @@
+# This is your practice R script
+# You can talk to your self in code via #
+# Please put all your code in here!
+# run line 5 
+# install.packges(tidyverse) 
+# It didnt do anything because it is commented out
+# now run line 8
+install.packages("tidyverse") # this might take a bit to run
+library(tidyverse)
+
+<- read.csv()
+
+
+ 
